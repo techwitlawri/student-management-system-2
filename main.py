0d3d4b33ddf7684007f77733e315f60732f718b1
Mmachi Ezeh <mmachilawri@gmail.com>
2024-09-12 12:18:52 +0100

    Add Toolbar

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel,\
     QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow,\
-    QTableWidget,  QTableWidgetItem, QDialog, QComboBox
+    QTableWidget,  QTableWidgetItem, QDialog, QComboBox, QToolBar
 import sys
-from PyQt6.QtGui import QAction
+from PyQt6.QtGui import QAction, QIcon
 import sqlite3
 
 
@@ -11,24 +11,42 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
+        self.setMinimumSize(800,600)
 
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
+        edit_menu_item = self.menuBar().addMenu("Edit")
 
 
-        add_student_action = QAction("Add Student", self)
+        add_student_action = QAction(QIcon("icons/add.png"), "Add Student", self)
         add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
         
+
+        search_action = QAction(QIcon("icons\search.png"), "Search", self)
+        edit_menu_item.addAction(search_action)
+        search_action.triggered.connect(self.search)
 
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
+
+
+        # create toolbar and add toolbar element
+        toolbar =QToolBar()
+        toolbar.setMovable(True)
+        self.addToolBar(toolbar)
+
+
+        toolbar.addAction(add_student_action)
+        toolbar.addAction(search_action)
+
         
 
     def load_data(self):
@@ -47,6 +65,11 @@ class MainWindow(QMainWindow):
         dialog = InsertDialog()
         dialog.exec()
 
+    def search(self):
+        dialog = SearchDialog()
+        dialog.exec()
+
+
 class InsertDialog(QDialog):
     def __init__(self):
         super().__init__()
@@ -55,6 +78,7 @@ class InsertDialog(QDialog):
         self.setFixedHeight(300)
 
         layout= QVBoxLayout()
+
         #add student name
         self.student_name = QLineEdit()
         self.student_name.setPlaceholderText("Name")
@@ -93,10 +117,46 @@ class InsertDialog(QDialog):
         connection.close()
         MainWindow.load_data()
 
+class SearchDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        # Set window title and size
+        self.setWindowTitle("Search Student")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        # Create layout and input widget
+        layout = QVBoxLayout()
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Create button
+        button = QPushButton("Search")
+        button.clicked.connect(self.search)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def search(self):
+        name = self.student_name.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT * FROM students WHERE name = ?", (name,))
+        row = list(result)[0]
+        print(row)
+        items = main_window.table.findItems("John Smith", Qt.MatchFlag.MatchFixedString)
+        for item in items:
+            print(item)
+            main_window.table.item(item.row(), 1).setSelected(True)
+
+        cursor.close()
+        connection.close()
+
 
 app= QApplication(sys.argv)
-MainWindow = MainWindow()
-MainWindow.show()
-MainWindow.load_data()
+main_window = MainWindow()
+main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
     
